cc8242dc3e1cf2e8a6d07c0d24f935414b9f1907
Clément Nonn <clement.nonn@worldline.com>
2020-06-23 15:53:35 +0200

    initial commit for project structure

---
+++ RunningOrder/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  RunningOrder
+//
+//  Created by Clément Nonn on 23/06/2020.
+//  Copyright © 2020 Worldline. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RunningOrderTests/RunningOrderTests.swift
@@ -0,0 +1,34 @@
+//
+//  RunningOrderTests.swift
+//  RunningOrderTests
+//
+//  Created by Clément Nonn on 23/06/2020.
+//  Copyright © 2020 Worldline. All rights reserved.
+//
+
+import XCTest
+@testable import RunningOrder
+
+class RunningOrderTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
