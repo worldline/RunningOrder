4b88670929a44c368c5066988465492153cc194f
Lucas Barbero <lucas.barbero@worldline.com>
2020-07-10 16:54:04 +0200

    Fix sidebar toolbaritem strange behaviour and addStory toolbar button

--- RunningOrder/CustomWindow.swift
+++ RunningOrder/CustomWindow.swift
@@ -13,24 +13,45 @@ import SwiftUI
 final class AppWindowController: NSWindowController, NSToolbarDelegate {
     func toolbarAllowedItemIdentifiers(_ toolbar: NSToolbar) -> [NSToolbarItem.Identifier] {
         [
-            NSToolbarItem.Identifier.toggleSidebar,
-            NSToolbarItem.Identifier.space,
-            NSToolbarItem.Identifier.flexibleSpace,
-            NSToolbarItem.Identifier.cloudSharing
+            .toggleSidebar,
+            .space,
+            .addStory,
+            .flexibleSpace,
+            .cloudSharing
         ]
     }
     func toolbarDefaultItemIdentifiers(_ toolbar: NSToolbar) -> [NSToolbarItem.Identifier] {
         [
-            NSToolbarItem.Identifier.toggleSidebar,
-            NSToolbarItem.Identifier.flexibleSpace,
-            NSToolbarItem.Identifier.cloudSharing
+            .toggleSidebar,
+            .addStory,
+            .flexibleSpace,
+            .cloudSharing
         ]
     }
 
     func toolbar(_ toolbar: NSToolbar, itemForItemIdentifier itemIdentifier: NSToolbarItem.Identifier, willBeInsertedIntoToolbar flag: Bool) -> NSToolbarItem? {
-        let item = NSToolbarItem(itemIdentifier: itemIdentifier)
 
-        return item
+        var toolbarItem: NSToolbarItem = NSToolbarItem()
+
+        if itemIdentifier == NSToolbarItem.Identifier.addStory {
+            toolbarItem = customToolbarButtonItem(itemForItemIdentifier: itemIdentifier.rawValue,
+                                                  label: NSLocalizedString("Add a story", comment: ""),
+                                                  paletteLabel: NSLocalizedString("Add a story", comment: ""),
+                                                  toolTip: NSLocalizedString("Add a story", comment: ""),
+                                                  iconImageName: NSImage.addTemplateName,
+                                                  action: #selector(addStoryActionPlaceHolder))!
+        }
+
+        return toolbarItem
+    }
+
+    func toolbarWillAddItem(_ notification: Notification) {
+        let item = notification.userInfo!["item"] as? NSToolbarItem
+        if let item = item {
+            if item.itemIdentifier == NSToolbarItem.Identifier.toggleSidebar {
+                item.action = #selector(toggleSidebar)
+            }
+        }
     }
 
     override func windowDidLoad() {
@@ -38,4 +59,44 @@ final class AppWindowController: NSWindowController, NSToolbarDelegate {
 
         window?.contentViewController = NSHostingController(rootView: MainView())
     }
+
+    func customToolbarButtonItem(
+        itemForItemIdentifier itemIdentifier: String,
+        label: String,
+        paletteLabel: String,
+        toolTip: String,
+        iconImageName: String,
+        action: Selector) -> NSToolbarItem? {
+
+        let toolbarItem = NSToolbarItem(itemIdentifier: NSToolbarItem.Identifier(rawValue: itemIdentifier))
+
+        let iconImage = NSImage(named: iconImageName)
+
+        let button = NSButton(frame: NSRect(x: 0, y: 0, width: 40, height: 40))
+        button.title = ""
+        button.image = iconImage
+        button.bezelStyle = .texturedRounded
+
+        button.action = action
+        toolbarItem.view = button
+        toolbarItem.label = label
+        toolbarItem.paletteLabel = paletteLabel
+        toolbarItem.toolTip = toolTip
+        toolbarItem.target = self
+
+        return toolbarItem
+    }
+
+// TODO change these lines in the future
+    @objc func toggleSidebar(_ sender: Any) {
+        ((window?.contentView?.subviews.first?.subviews.first?.subviews.first as? NSSplitView)?.delegate as? NSSplitViewController)?.toggleSidebar(self)
+    }
+
+    @objc func addStoryActionPlaceHolder() {
+        print("addStory toolbarbutton clicked")
+    }
+}
+
+private extension NSToolbarItem.Identifier {
+    static let addStory: NSToolbarItem.Identifier = NSToolbarItem.Identifier(rawValue: "AddStory")
 }
