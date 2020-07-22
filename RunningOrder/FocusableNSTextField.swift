c3bd80837013fedf2bb93441fda709214cab0b17
Lucas Barbero <lucas.barbero@worldline.com>
2020-07-22 15:19:36 +0200

    Inline list component with custom textfield

---
+++ RunningOrder/FocusableNSTextField.swift
@@ -0,0 +1,69 @@
+//
+//  FocusableNSTextField.swift
+//  RunningOrder
+//
+//  Created by Lucas Barbero on 23/07/2020.
+//  Copyright © 2020 Worldline. All rights reserved.
+//
+import AppKit
+import SwiftUI
+
+class FocusableNSTextField: NSTextField {
+    var onFocusChange: (Bool) -> Void = { _ in }
+
+    override func becomeFirstResponder() -> Bool {
+        let textView = window?.fieldEditor(true, for: nil) as? NSTextView
+        textView?.insertionPointColor = NSColor.blue // TODO Replace with accent color
+        onFocusChange(true)
+        return super.becomeFirstResponder()
+    }
+}
+
+struct FocusableNSTextFieldRepresentable: NSViewRepresentable {
+
+    let placeholder: String
+
+    @Binding var value: String
+    @Binding var isFocused: Bool
+
+    let onCommit: () -> Void
+
+    func makeNSView(context: Context) -> NSTextField {
+        let textField = FocusableNSTextField()
+        textField.delegate = context.coordinator
+        textField.placeholderString = placeholder
+        textField.backgroundColor = NSColor.clear
+        textField.focusRingType = .none
+        textField.isBordered = false
+        textField.onFocusChange = { isFocused in
+            self.isFocused = isFocused
+        }
+        return textField
+    }
+
+    func updateNSView(_ nsView: NSTextField, context: Context) {
+        nsView.stringValue = value
+    }
+
+    func makeCoordinator() -> FocusableNSTextFieldRepresentable.Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, NSTextFieldDelegate {
+        var parent: FocusableNSTextFieldRepresentable
+
+        init(_ textFieldContainer: FocusableNSTextFieldRepresentable) {
+            self.parent = textFieldContainer
+        }
+
+        func controlTextDidChange(_ obj: Notification) {
+            guard let textField = obj.object as? NSTextField else { return }
+            self.parent.value = textField.stringValue
+        }
+
+        func controlTextDidEndEditing(_ obj: Notification) {
+            self.parent.isFocused = false
+            self.parent.onCommit()
+        }
+    }
+}
---
+++ RunningOrder/InlineEditableList.swift
@@ -0,0 +1,96 @@
+//
+//  InlineTexField.swift
+//  RunningOrder
+//
+//  Created by Lucas Barbero on 20/07/2020.
+//  Copyright © 2020 Worldline. All rights reserved.
+//
+
+import SwiftUI
+import AppKit
+
+/// Display an inline editable list of string element
+struct InlineEditableList: View { // Change name ???
+
+    let title: String
+
+    let placeholder: String
+
+    @Binding var values: [String]
+    @State private var hovered = false
+
+    var body: some View {
+        VStack {
+
+            HStack(alignment: .top) {
+                Text(title).foregroundColor(.gray)
+                Spacer()
+                AddButton(action: addTextfieldValue)
+            }
+            .padding(.horizontal, 10)
+
+            ForEach(values.indices, id: \.self) { index in
+                ZStack(alignment: .trailing) {
+                    FocusableTextField(placeholder, value: Binding(
+                        get: { return values[index] },
+                        set: { newValue in return self.values[index] = newValue}
+                    ),
+                    onCommit: {
+                        //sanity check to prevent some out of bounds exception
+                        if index < values.count && values[index].isEmpty {
+                            values.remove(at: index)
+                        }
+                    })
+                    DeleteButton(action: {
+                        values.remove(at: index)
+                    }).padding(.horizontal, 10)
+                }
+            }
+        }
+        .padding()
+        .background(hovered ? Color("gray1") : Color.white)
+        .cornerRadius(5)
+        .onHover { isHovered in
+            withAnimation(.easeInOut) {
+                self.hovered = isHovered
+            }
+        }
+    }
+
+    private func addTextfieldValue() {
+        values.append("")
+    }
+}
+
+struct InlineTexField_Previews: PreviewProvider {
+    static var previews: some View {
+        InlineEditableList(title: "A Title", placeholder: "A Placeholder for all my fields", values: .constant(["value1", "value2"]))
+    }
+}
+
+struct FocusableTextField: View {
+
+    let placeholder: String
+
+    @State private var isFocused = false
+    @Binding var value: String
+
+    let onCommit: () -> Void
+
+    init(_ placeholder: String, value: Binding<String>, onCommit: @escaping () -> Void) {
+        self.placeholder = placeholder
+        self._value = value
+        self.onCommit = onCommit
+    }
+
+    var body: some View {
+        FocusableNSTextFieldRepresentable(placeholder: placeholder, value: $value, isFocused: $isFocused, onCommit: onCommit)
+            .padding(.all, 10)
+            // on focus background
+            .background(Capsule().foregroundColor(Color.white.opacity(isFocused ? 1 : 0)))
+            // on focus border
+            .overlay(Capsule().strokeBorder(Color.blue, lineWidth: 1.0, antialiased: true).opacity(isFocused ? 1 : 0))
+            .animation(.easeIn)
+            .focusable()
+    }
+}
